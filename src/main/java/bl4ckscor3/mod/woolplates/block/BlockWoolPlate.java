4ea6e36a46e8f30741623dc744a5b8e0415a16f4
bl4ckscor3 <donalddani9612@gmx.de>
2018-10-12 12:18:15 +0200

    wool plates now negate a tiny bit of fall damage

--- src/main/java/bl4ckscor3/mod/woolplates/block/BlockWoolPlate.java
+++ src/main/java/bl4ckscor3/mod/woolplates/block/BlockWoolPlate.java
@@ -4,6 +4,9 @@ import bl4ckscor3.mod.woolplates.WoolPlates;
 import net.minecraft.block.BlockPressurePlate;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockWoolPlate extends BlockPressurePlate
 {
@@ -15,5 +18,11 @@ public class BlockWoolPlate extends BlockPressurePlate
 		setTranslationKey(WoolPlates.PREFIX + name);
 		setHardness(0.5F);
 		setSoundType(SoundType.CLOTH);
+	}
+
+	@Override
+	public void onFallenUpon(World worldIn, BlockPos pos, Entity entityIn, float fallDistance)
+	{
+		super.onFallenUpon(worldIn, pos, entityIn, fallDistance * 0.8F);
 	}
 }
