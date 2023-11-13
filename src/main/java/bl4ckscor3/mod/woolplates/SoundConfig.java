a0bfbf258f46327a7dad878eab9c84d751af7ae9
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-11-13 13:36:05 +0100

    update to 1.20.2 using NeoForge

--- src/main/java/bl4ckscor3/mod/woolplates/SoundConfig.java
+++ src/main/java/bl4ckscor3/mod/woolplates/SoundConfig.java
@@ -1,23 +1,22 @@
 package bl4ckscor3.mod.woolplates;
 
+import net.neoforged.neoforge.common.ModConfigSpec;
+import net.neoforged.neoforge.common.ModConfigSpec.BooleanValue;
 import org.apache.commons.lang3.tuple.Pair;
 
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
-
 public class SoundConfig {
-	public static final ForgeConfigSpec CONFIG_SPEC;
+	public static final ModConfigSpec CONFIG_SPEC;
 	public static final SoundConfig CONFIG;
 	public final BooleanValue enableSound;
 
 	static {
-		Pair<SoundConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(SoundConfig::new);
+		Pair<SoundConfig, ModConfigSpec> specPair = new ModConfigSpec.Builder().configure(SoundConfig::new);
 
 		CONFIG_SPEC = specPair.getRight();
 		CONFIG = specPair.getLeft();
 	}
 
-	SoundConfig(ForgeConfigSpec.Builder builder) {
+	SoundConfig(ModConfigSpec.Builder builder) {
 		//@formatter:off
 		enableSound = builder
 				.comment("true if the wool pressure plates should play a sound when stepped on/off, false otherwhise")
--- src/main/java/bl4ckscor3/mod/woolplates/WoolPlates.java
+++ src/main/java/bl4ckscor3/mod/woolplates/WoolPlates.java
@@ -15,16 +15,16 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.PressurePlateBlock.Sensitivity;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ForgeRegistries.Keys;
-import net.minecraftforge.registries.RegisterEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+import net.neoforged.fml.common.Mod.EventBusSubscriber.Bus;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.ForgeRegistries;
+import net.neoforged.neoforge.registries.ForgeRegistries.Keys;
+import net.neoforged.neoforge.registries.RegisterEvent;
 
 @Mod(WoolPlates.MODID)
 @EventBusSubscriber(bus = Bus.MOD)
