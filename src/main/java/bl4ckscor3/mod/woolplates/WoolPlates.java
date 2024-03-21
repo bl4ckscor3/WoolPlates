799fb6dc8bd4f5de4018f6d0152a0fe19ad2dfc7
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-03-21 19:26:27 +0100

    make sculk sensors ignore wool pressure plates

--- src/main/java/bl4ckscor3/mod/woolplates/WoolPlates.java
+++ src/main/java/bl4ckscor3/mod/woolplates/WoolPlates.java
@@ -34,7 +34,7 @@ public class WoolPlates {
 	public static final BlockSetType WOOL_PLATES_BLOCK_SET_TYPE = BlockSetType.register(new BlockSetType(MODID + ":wool", true, SoundType.WOOL, SoundEvents.EMPTY, SoundEvents.EMPTY, SoundEvents.EMPTY, SoundEvents.EMPTY, SoundEvents.WOODEN_PRESSURE_PLATE_CLICK_OFF, SoundEvents.WOODEN_PRESSURE_PLATE_CLICK_ON, SoundEvents.EMPTY, SoundEvents.EMPTY));
 
 	public WoolPlates() {
-		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SoundConfig.CONFIG_SPEC);
+		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Configuration.CONFIG_SPEC);
 	}
 
 	@SubscribeEvent
@@ -71,7 +71,7 @@ public class WoolPlates {
 		return new ResourceLocation(MODID, "wool_plate_" + color.name().toLowerCase());
 	}
 
-	private static enum Color {
+	public enum Color {
 		WHITE,
 		LIGHT_GRAY,
 		GRAY,
